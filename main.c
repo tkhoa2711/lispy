3862e50994b2906a638adff8f649f785f0d06e78
Khoa Le <ltkhoa2711@gmail.com>
2021-09-14 21:38:57 +1000

    feat: allow the code to run on Windows

--- main.c
+++ main.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+// Fake readline function
+char* readline(char* prompt) {
+    fputs(prompt, stdout);
+    fgets(buffer, 2048, stdin);
+    char* cpy = malloc(strlen(buffer)+1);
+    strcpy(cpy, buffer);
+    cpy[strlen(cpy)-1] = '\0';
+    return cpy;
+}
+
+// Fake add_history function
+void add_history(char* unused) {}
+
+#else
 #include <editline/readline.h>
+#endif
 
 int main(int argc, char *argv[]) {
     puts("Press Ctrl+c to Exit\n");
