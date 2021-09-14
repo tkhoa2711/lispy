a822270ed454a730d68e45600a9d7a69392fcc6a
Khoa Le <ltkhoa2711@gmail.com>
2021-09-14 21:14:52 +1000

    feat: create a simple interactive prompt

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
+// Declare a buffer for user input of size 2048 characters
+static char input[2048];
+
 int main(int argc, char *argv[]) {
-    puts("Hello, World!");
+    puts("Press Ctrl+c to Exit\n");
+
+    while (1) {
+        // Output the prompt
+        fputs("lispy> ", stdout);
+
+        // Read the user input (of maximum 2048 characters)
+        fgets(input, 2048, stdin);
+
+        // Simply echo the user input back for now
+        printf("You said: %s", input);
+    }
+
     return 0;
 }
